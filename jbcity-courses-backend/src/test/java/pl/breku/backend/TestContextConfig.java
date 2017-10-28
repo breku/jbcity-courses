4bb1577e509958dfa84d43dec950afeb50d5c93a
brekol <brekol@gmail.com>
2017-10-28 18:41:54 +0200

    fix testow

--- jbcity-courses-backend/src/test/java/pl/breku/backend/TestContextConfig.java
+++ jbcity-courses-backend/src/test/java/pl/breku/backend/TestContextConfig.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.ComponentScan;
  * Created by breku on 25.10.17.
  */
 @ComponentScan(basePackages = {
-		"pl.breku.backend"
+		"pl.breku.backend.course",
+		"pl.breku.backend.file",
 })
 public class TestContextConfig {
 }
